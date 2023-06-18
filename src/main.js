0cc2cc3daff1de85a993ac41c02a136ba2c8847c
Magomed-R <magomed-radzhabov@internet.ru>
2023-06-18 19:01:12 +0300

    domain change 1.1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router.js'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://localhost:3005';
 axios.defaults.headers.common['Content-type'] = 'application/json'
 
 import dayjs from 'dayjs'
